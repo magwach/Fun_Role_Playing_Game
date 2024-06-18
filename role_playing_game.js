13ffc99427818bb2339d0e8972b92ddee6ccc178
magwach <emmanuelmagothe@gmail.com>
2024-06-18 17:38:16 +0300

    updated the code

--- role_playing_game.js
+++ role_playing_game.js
@@ -9,3 +9,56 @@ let inventory = ["stick"];
 const button1 = document.querySelector("#button1");
 const button2 = document.querySelector("#button2");
 const button3 = document.querySelector("#button3");
+const text = document.querySelector("#text");
+const healthText = document.querySelector("#healthText");
+const goldText = document.querySelector("#goldText");
+const monsterStats = document.querySelector("#monsterStats");
+const monsterName = document.querySelector("#monsterName");
+const xpText = document.querySelector("#xpText");
+const monsterHealthText = document.querySelector("#monsterHealth");
+
+function goTown()
+{
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    text.innerText = "You enter the store.";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+}
+
+function goStore()
+{
+    button1.innerText = "Buy 10 health (10 gold)";
+    button2.innerText = "Buy weapon (30 gold)";
+    button3.innerText = "Go to town square";
+    text.innerText = "You enter the store.";
+    button1.onclick = buyHealth;
+    button2.onclick = buyWeapon;
+    button3.onclick = goTown;
+}
+
+function goCave() {
+
+}
+
+function fightDragon()
+{
+
+}
+
+function buyHealth()
+{
+
+}
+
+function buyWeapon()
+{
+
+}
+
+//initializing the buttons
+button1.onclick = goStore;
+button2.onclick = goCave;
+button3.onclick = fightDragon;
