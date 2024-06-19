0426c027e2352e2fb7d784f2fd3b1c2f85d740fd
magwach <emmanuelmagothe@gmail.com>
2024-06-19 22:01:34 +0300

    updated the code

--- role_playing_game.js
+++ role_playing_game.js
@@ -59,6 +59,12 @@ const locations =
         "button text" : ["REPLAY?", "REPLAY?", "REPLAY?"],
         "button functions" : [restart, restart, restart],
         text :"You defeat the dragon! YOU WIN THE GAME! &#x1F389;"
+    },
+    {
+        name : "easter egg",
+        "button text" : ["2", "8", "Go to town square?"],
+        "button functions" : [pickTwo, pickEight, goTown],
+        text :"You find a secret game. Pick a number above. Ten numbers will be randomly chosen between 0 and 10. If the number you choose matches one of the random numbers, you win!"
     }
 ];
 
@@ -230,7 +236,14 @@ function attack()
     text.innerText = "The "+ monsters[fighting].name +" attacks.";
     text.innerText += " You attack it with your "+ weapons[currentWeaponIndex].name +".";
     health -= getMonsterAttackValue(monsters[fighting].level);
-    monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+    if (isMonsterHit)
+    {
+        monsterHealth -= weapons[currentWeaponIndex].power + Math.floor(Math.random() * xp) + 1;
+    }
+    else
+    {
+        text.innerText += " You miss.";
+    }
     healthText.innerText = health;
     monsterHealthText.innerText = monsterHealth;
     if (health <= 0)
@@ -248,7 +261,26 @@ function attack()
            defeatMonster();
        }
     }
+    if (Math.random() <= .1 && inventory.length !== 1)
+    {
+        text.innerText += " Your "+ inventory.pop() + " breaks.";
+        currentWeaponIndex--;
+    }
 
+
+
+}
+
+function getMonsterAttackValue(level)
+{
+    const hit = (level * 5) - (Math.floor(Math.random() * xp));
+    return hit > 0 ? hit : 0;
+
+}
+
+function isMonsterHit()
+{
+    return Math.random() > .2 || health < 20;
 }
 
 function dodge()
@@ -289,4 +321,37 @@ function restart()
 function winGame()
 {
     update(locations[6]);
+}
+
+function easterEgg()
+{
+    update(locations[7]);
+}
+
+function pick(guess)
+{
+const numbers = [];
+    while (numbers.length < 10)
+    {
+        numbers.push(Math.floor(Math.random() * 11));
+    }
+    text.innerText = "You picked "+ guess +". Here are the random numbers:\n";
+    for (let i = 0; i < 10; i++)
+    {
+        text.innerText += numbers[i] + "\n";
+    }
+    if (numbers.includes(guess))
+    {
+
+    }
+}
+
+function pickTwo()
+{
+    pick(2);
+}
+
+function pickEight()
+{
+    pick(8);
 }
