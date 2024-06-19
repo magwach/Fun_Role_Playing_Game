6916cd56f661db81efb4782fe8e43ba8c40a51c8
magwach <emmanuelmagothe@gmail.com>
2024-06-19 20:34:52 +0300

    updated the code

--- role_playing_game.js
+++ role_playing_game.js
@@ -41,6 +41,12 @@ const locations =
         "button text" : ["Attack", "Dodge", "Run"],
         "button functions" : [attack, dodge, goTown],
         text : "You are fighting a monster."
+    },
+    {
+        name : "kill monster",
+        "button text" : ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions" : [goTown, goTown, goTown],
+        text :'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
     }
 ];
 
@@ -93,6 +99,7 @@ button3.onclick = fightDragon;
 
 function update(location)
 {
+    monsterStats.style.display = "none";
     button1.innerText = location["button text"][0];
     button2.innerText = location["button text"][1];
     button3.innerText = location["button text"][2];
@@ -233,10 +240,16 @@ function dodge()
 
 function defeatMonster()
 {
+    gold += Math.floor(monsters[fighting].level * 6.7);
+    xp += monsters[fighting].level;
+    goldText.innerText = gold;
+    xpText.innerText = xp;
+    update(locations[4]);
 
 }
 
 function lose()
 {
+    update(locations[5]);
 
 }
